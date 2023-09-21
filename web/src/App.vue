14c1c7d6b3e3e507c9fb74422518c0e77afd7acd
unknown <2411577993@qq.com>
2023-09-21 15:30:58 +0800

    前端部分代码

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,34 +1,19 @@
 <template>
-  <div>
-   <div>bot昵称: {{ bot_name }}</div>
-   <div>bot战力: {{ bot_rating }}:</div>
-  </div>
-  <router-view/>
+  <NavBar/>
+  <router-view></router-view>
 </template>
 
 <script>
-import $ from 'jquery';
-import { ref } from 'vue';
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+import { RouterView } from 'vue-router';
 
 export default {
-  name:"App",
-  setup:() =>{
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    $.ajax({
-      url:"http://localhost:3000/pk/getBotInfo/",
-      type: "get",
-      success: resp=>{
-       bot_name.value = resp.name;
-       bot_rating.value = resp.rating;
-      }
-    });
-    return {
-      bot_name,
-      bot_rating
-    }
-  }
+  components:{
+    NavBar,
+    RouterView
+}
 }
 </script>
 

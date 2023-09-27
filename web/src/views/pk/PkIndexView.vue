1723743a43528ad402918667628390e7a16ada2c
unknown <2411577993@qq.com>
2023-09-27 16:24:20 +0800

    创建前端与地图

--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -1,14 +1,16 @@
 <template>
-  <ContentField>对战</ContentField>
+ <PlayGround>
+  dasd
+ </PlayGround>
 </template>
 
 <script>
 
-import ContentField from "../../components/ContentField.vue"
+import PlayGround from '../../components/PlayGround.vue'
 
 export default{
     components:{
-        ContentField
+        PlayGround
     }
 }
 </script>
